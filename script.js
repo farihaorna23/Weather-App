619c59e0ac4e152c969936fdd72e739bf36cf7f8
farihaorna23 <farihaorna@gmail.com>
2022-10-16 18:09:32 -0400

    getting value from the input

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //
+let city = document.getElementById("city");
+let form = document.getElementById("submitForm");
+console.log(city);
+
+form.addEventListener("submit", Submitted);
+
+function Submitted(e) {
+  e.preventDefault();
+  console.log("submitted");
+  console.log(city.value);
+}
