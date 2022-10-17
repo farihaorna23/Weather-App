dfffe43c409ffddce445cc549e6e5c918ec8d4b6
farihaorna23 <farihaorna@gmail.com>
2022-10-17 15:18:35 -0400

    Implemented Search and Fetch Api function

--- src/script.js
+++ src/script.js
@@ -10,13 +10,15 @@ function Search(e) {
   e.preventDefault();
   let cityName = city.value;
   fetchApi(cityName);
+  city.value = "";
 }
 
 async function fetchApi(city) {
   let cityName = city;
   try {
-    let response = await fetch(`${weatherUrl}q={cityName}&appid={apiKey}`);
-    console.log(response);
+    let response = await fetch(`${weatherUrl}q=${cityName}&appid=${apiKey}`);
+    let data = await response.json();
+    console.log(data);
   } catch (err) {
     console.error(err);
   }
