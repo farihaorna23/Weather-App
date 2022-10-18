b25eb24a56e7090d89e54996bdf19cd6ad94a1ee
farihaorna23 <farihaorna@gmail.com>
2022-10-17 20:53:52 -0400

    Created the function tp update the display weather box

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
-import { city, form, searchbtn, apiKey } from "./variables.js";
-console.log(city);
+import { city, form, searchbtn, apiKey, weatherBox } from "./variables.js";
 
 form.addEventListener("submit", Search);
 searchbtn.addEventListener("click", Search);
@@ -20,8 +19,31 @@ async function fetchApi(city) {
       `${weatherUrl}q=${cityName}&appid=${apiKey}&units=metric`
     );
     let data = await response.json();
-    console.log(data);
+    let weather = data.weather[0].main;
+    let temperature = data.main.temp;
+    let description = data.weather[0].description;
+    updateWeather(cityName, weather, temperature, description);
   } catch (err) {
     console.error(err);
   }
+}
+
+function updateWeather(city, weather, temperature, description) {
+  let ul = document.createElement("ul");
+  ul.className = "list-group";
+  let li1 = document.createElement("li");
+  li1.textContent = `City: ${city}`;
+  li1.className = "list-group-item";
+  let li2 = document.createElement("li");
+  li2.textContent = `Weather: ${weather}`;
+  li2.className = "list-group-item";
+  let li3 = document.createElement("li");
+  li3.textContent = `Description: ${description}`;
+  li3.className = "list-group-item";
+  let li4 = document.createElement("li");
+  li4.textContent = `Temperature: ${temperature}`;
+  li4.className = "list-group-item";
+  ul.append(li1, li2, li3, li4);
+  weatherBox.appendChild(ul);
+  console.log(weatherBox);
 }
--- src/variables.js
+++ src/variables.js
@@ -1,4 +1,5 @@
 export let city = document.getElementById("city");
 export let form = document.getElementById("submitForm");
 export let searchbtn = document.getElementById("searchbtn");
+export let weatherBox = document.getElementById("weatherBox");
 export let apiKey = "368e109c93e37d28d5ba1fe761361bbd";
